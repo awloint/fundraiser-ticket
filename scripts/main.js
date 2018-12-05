e9f939fad576eec2ad8f306e290981a9ee3dc077
Benson Imoh, ST <benson@stbensonimoh.com>
2018-12-05 16:34:38 +0100

    * Add font awesome, add script to change the button to loading on submit * Add font awesome CDN Link. * add script to change the button to loading on submit * put form data into variables * start AJAX request to see if user has already registered

--- scripts/main.js
+++ scripts/main.js
@@ -26,4 +26,34 @@ $(document).ready(function (){
         val = val.toLowerCase();
     });
 
+    $('form').submit(function(event){
+        event.preventDefault();
+        
+        // Make the submit button load
+        $('button').removeClass('btn-danger');
+        $('button').toggleClass('btn-primary');
+        $('button').html('Loading <span class="spinner"></span><i class="fa fa-spinner fa-spin"></i></span>');
+
+        // put form data into variables
+        var firstName = $.trim(document.getElementById('firstName').value);
+        var lastName = $.trim(document.getElementById('lastName').value);
+        var email = $.trim(document.getElementById('email').value);
+        var phone = $.trim(document.getElementById('phone').value);
+
+        var dataString = 'firstName=' + firstName + '&lastName=' + lastName + '&email=' + email + '&phone=' + phone;
+
+        // check to see if the person has already registered
+        $.ajax({
+            type: 'POST',
+            url: 'buy.php',
+            data: dataString,
+            success: function(result) {
+                if(result == "user exists") {
+                    swal("Already Paid", "You have already paid for the Fundraiser Ticket.", "info");
+                }
+            }
+        });
+
+    });
+
 });
